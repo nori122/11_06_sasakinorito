48143bbea78294fdb4a244e22aa8b439b7994baf
nori122 <norito.sasaki.jp@gmail.com>
2020-06-16 01:12:47 +0900

    search_formed

---
+++ search.php
@@ -0,0 +1,59 @@
+<!---------------------
+     php 要素
+--------------------->
+<?PHP
+session_start();
+include("functions.php");
+check_session_id();
+
+
+// var_dump($_SESSION);
+// exit();
+
+
+?>
+
+
+
+<!---------------------
+     HTML 要素
+--------------------->
+<!DOCTYPE html>
+<html lang='ja'>
+
+<head>
+    <meta charset='UTF-8'>
+    <link rel='stylesheet' href='styles.css'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+
+    <title>search</title>
+</head>
+
+<body>
+    <!-- マッサージメニューを選択 -->
+    <form action="search_act.php" method="POST">
+        <fieldset>
+            <legend>メニュー</legend>
+            <p><label><input type="checkbox" name="menu" value="menu_foot">フットマッサージ</label></p>
+            <p><label><input type="checkbox" name="menu" value="menu_body">ボディマッサージ</label></p>
+            <p><label><input type="checkbox" name="menu" value="menu_head">ヘッドマッサージ</label></p>
+            <p><label><input type="checkbox" name="menu" value="menu_oil">オイルマッサージ</label></p>
+        </fieldset>
+
+        <!-- 価格レンジを選択 -->
+        <fieldset>
+            <legend>Price</legend>
+            <input type='tel' name='price' id='price_min'>
+            <span>円~ </span>
+            <input type='tel' name='price' id='price_max'>
+            <span>円</span>
+        </fieldset>
+
+        <!-- 送信ボタン -->
+        <button>検索</button>
+    </form>
+
+
+</body>
+
+</html>
